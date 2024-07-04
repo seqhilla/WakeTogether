05a4c75f44e036f60dfdb157114f80116aa9cef9
Muhammet Gürkan Cingöz <muhammetgurkan.cingoz@intertech.com.tr>
2024-07-04 14:35:25 +0300

    Login with google.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,45 @@
 import 'dart:async';
 
 import 'package:alarm/alarm.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:waketogether/screens/home_page.dart';
+import 'package:waketogether/screens/pre_login_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Alarm.init();
-
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Widget _defaultHome = SignInPage();
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        setState(() {
+          _defaultHome = SignInPage();
+        });
+      } else {
+        setState(() {
+          _defaultHome = const MyHomePage(title: 'WakeTogether');
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +49,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'WakeTogether'),
+      home: _defaultHome,
     );
   }
 }
---
+++ lib/screens/pre_login_screen.dart
@@ -0,0 +1,54 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class SignInPage extends StatefulWidget {
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<UserCredential?> _handleSignIn() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) {
+        // The user canceled the sign-in
+        return null;
+      }
+
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      return await _auth.signInWithCredential(credential);
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign in'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Sign in with Google'),
+          onPressed: () {
+            _handleSignIn()
+                .then((UserCredential? user) => print(user))
+                .catchError((e) => print(e));
+          },
+        ),
+      ),
+    );
+  }
+}
