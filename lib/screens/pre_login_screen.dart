cd34e4dd194d7fcb6d8d05febd457de41023806c
M.Gürkan Cingöz <gurkancingoz1@gmail.com>
2025-02-27 00:16:57 +0300

    Revert "res fix."
    This reverts commit 49f7e4186efaf5d8c54835cc228d9c6635d53e82.

--- lib/screens/pre_login_screen.dart
+++ lib/screens/pre_login_screen.dart
@@ -61,15 +61,15 @@ class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
 
-    final res = GeneralUtils.resources(context);
+    //final res = GeneralUtils.resources(context);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(res.sign_in),
+        title: Text("res.sign_in"),
       ),
       body: Center(
         child: ElevatedButton(
-          child: Text(res.sign_in_google),
+          child: Text("res.sign_in_google"), //TODO: Furkan burayı yap
           onPressed: () {
             _handleSignIn()
                 .then((UserCredential? user) => print(user))
